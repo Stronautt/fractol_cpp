efa7ecd74cfa2deca637d039de83937e5f0e9851
Pavlo Hrytsenko <pavlo.hrytsenko@volvocars.com>
2019-09-23 12:11:55 +0300

    Initial

---
+++ source/fractol.cpp
@@ -0,0 +1,30 @@
+/*
+ * This file is part of Fractol.
+
+ * Fractol is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Fractol is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Fractol. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Author: Pavlo Hrytsenko
+*/
+
+#include "fractol.hpp"
+
+namespace cozz {
+
+Fractol::Fractol(int argc, char **argv) {}
+
+uint8_t Fractol::Run() {
+    while (1) {}
+}
+
+}  // namespace cozz
---
+++ source/main.cpp
@@ -0,0 +1,26 @@
+/*
+ * This file is part of Fractol.
+
+ * Fractol is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Fractol is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Fractol. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Author: Pavlo Hrytsenko
+*/
+
+#include "fractol.hpp"
+
+int main(int argc, char **argv) {
+    cozz::Fractol application(argc, argv);
+
+    return application.Run();
+}
